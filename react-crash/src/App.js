0db6ce904d137657113417c14fa3b99693e83807
Jaseela99 <jaseelachittur@gmail.com>
2022-08-24 19:11:23 +0530

    added tasks and task

--- react-crash/src/App.js
+++ react-crash/src/App.js
@@ -1,8 +1,20 @@
+import React from "react"
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 function App() {
+  const [tasks,setTasks]=React.useState([])
+  //Delete Task
+  const deleteTask=(id)=>{
+    setTasks(tasks.filter((task)=>task.id!== id))
+  }
+  //toggle reminder
+  const toggleReminder=(id)=>{
+    setTasks(tasks.map((task)=>task.id===id ? {...task,reminder:!task.reminder}: task))
+  }
   return (
     <div className="App">
       <Header />
+      {tasks.length>0?(<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>):("No tasks to show")}
     </div>
   );
 }
