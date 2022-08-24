203afb6b1939785569fd6b0bb51d0c3f6c43b1f8
Jaseela99 <jaseelachittur@gmail.com>
2022-08-24 18:36:01 +0530

    added button

--- react-crash/src/components/Header.js
+++ react-crash/src/components/Header.js
@@ -1,9 +1,36 @@
-import React from 'react'
 
-const Header = () => {
+
+/* const Header = (props) => {
   return (
-    <div>Header</div>
-  )
+    // <div style={{color:"red",backgroundColor:"black"}}>{props.title}</div>
+  <div  style={Style}>{props.title}</div>
+    ) 
+}*/
+/* 
+
+const Style ={
+    color:"red"
+} */
+import Button from "./Button"
+import PropTypes from "prop-types"
+
+const Header = ({title}) => {
+    const onClick=()=>{
+       console.log("click") 
+    }
+  return (
+    
+  <header className="header">
+    <h1>{title}</h1>
+ <Button onClick={onClick} color="green" text="Add"/>
+    </header>
+    )
+}
+Header.defaultProps={
+    title:"Task Tracker"
+}
+Header.defaultPropTypes={
+    title:PropTypes.string
 }
 
 export default Header
